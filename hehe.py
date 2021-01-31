5cf506e48c6a4ed913dfb7b03af597f745bc48df
GTFOvO <294942509@qq.com>
2021-02-01 03:55:15 +0800

    hehe

--- hehe.py
+++ hehe.py
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
 print("hello world!!")
-=======
->>>>>>> 7d11831f68133d876cc206e2035b6f7aea27f09c
+print("hello world!!")
+print("hello world!!")
+print("hello world!!")
+print("hello world!!")
 print("hello world!!")
